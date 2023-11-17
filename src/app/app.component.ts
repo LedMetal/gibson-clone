db5fff92904a44456009e60a9fce71e826613a48
Abdul Sadiq <sadiq9707336@gmail.com>
2023-11-17 18:00:16 -0500

    create Header, Footer and PageLayout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { PageLayoutComponent } from './core/components/page-layout/page-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, PageLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gibson-clone';
-}
+export class AppComponent {}
