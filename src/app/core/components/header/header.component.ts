34fa1c6c74884952cca9f73b7d24c6f1ead20aea
Abdul Sadiq <sadiq9707336@gmail.com>
2023-11-17 19:21:37 -0500

    create top-bar within Header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatMenuModule, MatButtonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  faChevronDown = faChevronDown;
 
+  languageOptions: TLanguageOption[] = [
+    { title: 'United States', image: 'usa-flag.svg' },
+    { title: 'Belgie Be', image: 'belgium-flag.jfif' },
+    { title: 'Belgique Fr', image: 'belgium-flag.jfif' },
+    { title: 'Deutschland', image: 'germany-flag.png' },
+    { title: 'Espana', image: 'spanish-flag.png' },
+    { title: 'France', image: 'france-flag.png' },
+    { title: 'Ireland', image: 'ireland-flag.jpg' },
+    { title: 'Italia', image: 'italy-flag.png' },
+    { title: 'Nederland', image: 'nederlands-flag.png' },
+    { title: 'Osterreich', image: 'austria-flag.jpg' },
+    { title: 'UK', image: 'uk-flag.png' },
+  ];
 }
+
+type TLanguageOption = {
+  title: string;
+  image: string;
+};
