8fab9bb8270adb11bc6c879386ef3622492e8245
Abdul Sadiq <sadiq9707336@gmail.com>
2023-11-17 23:41:20 -0500

    create nav-bar section of Header and implement mat-menu system for nav links

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatMenuModule, MatButtonModule, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    RouterModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @ViewChildren(MatMenuTrigger) triggers: QueryList<MatMenuTrigger> | null =
+    null;
+
   faChevronDown = faChevronDown;
+  faCartShopping = faCartShopping;
 
   languageOptions: TLanguageOption[] = [
     { title: 'United States', image: 'usa-flag.svg' },
@@ -28,6 +40,12 @@ export class HeaderComponent {
     { title: 'Osterreich', image: 'austria-flag.jpg' },
     { title: 'UK', image: 'uk-flag.png' },
   ];
+
+  toggleMenu(trigger: MatMenuTrigger): void {
+    this.triggers?.forEach((trigger: MatMenuTrigger) => trigger.closeMenu());
+
+    trigger.openMenu();
+  }
 }
 
 type TLanguageOption = {
