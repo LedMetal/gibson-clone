b56c17e19790e1bd84df7a868cd104d704d2b174
Abdul Sadiq <sadiq9707336@gmail.com>
2023-11-25 04:05:56 -0500

    implement socials-section of Footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronRight, faComments } from '@fortawesome/free-solid-svg-icons';
+import {
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faFacebookF,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  faChevronRight = faChevronRight;
+  faComments = faComments;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
+  faFacebookF = faFacebookF;
 }
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronDown,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons';
 import { RouterModule } from '@angular/router';
 import { TLanguageOption } from '../../../shared/types/language-option';
-import { TMenuItem, TNavConfig } from '../../../shared/types/nav-config';
+import { TNavConfig } from '../../../shared/types/nav-config';
 import { NavbarServiceService } from '../../../data/services/navbar-service.service';
 import { OnInit } from '@angular/core';
 import { LanguageServiceService } from '../../../data/services/language-service.service';
