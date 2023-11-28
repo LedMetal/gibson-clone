fe0856b56ba373c995de755193a4dfcc5dafbde4
Abdul Sadiq <sadiq9707336@gmail.com>
2023-11-27 23:23:48 -0500

    recreate LanguageConfig and NavbarConfig Services

--- src/app/data/services/language-config.service.ts
+++ src/app/data/services/language-config.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TLanguageOption } from '../../shared/types/language-option';
 import languageData from '../json/language-config.json';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LanguageServiceService {
+export class LanguageConfigService {
   getLanguageOptions(): Observable<TLanguageOption[]> {
     return of(languageData);
   }
--- src/app/data/services/navbar-config.service.ts
+++ src/app/data/services/navbar-config.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TNavConfig } from '../../shared/types/nav-config';
 import navBarData from '../json/navbar-config.json';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NavbarServiceService {
+export class NavbarConfigService {
   getNavConfig(): Observable<TNavConfig[]> {
     return of(navBarData);
   }
